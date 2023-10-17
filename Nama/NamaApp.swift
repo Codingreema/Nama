4e3371532f10e3fc03b2408acf08af04d71f1a74
Codingreema <dimaabd383@gmail.com>
2023-10-17 09:56:50 +0300

    Initial Commit

---
+++ Nama/NamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamaApp.swift
+//  Nama
+//
+//  Created by Rimah on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
